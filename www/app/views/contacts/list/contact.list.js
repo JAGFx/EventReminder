ab2a8a7b0df966adad774d0e4d7568a1c0432aa1
JAGFx <contact@jagfx.fr>
2017-04-08 19:49:17 +0200

    Feature Event: Add support to pin contact from app

--- www/app/views/contacts/list/contact.list.js
+++ www/app/views/contacts/list/contact.list.js
@@ -8,12 +8,28 @@ angular
 	.module( 'er.contact.list', [ 'ui.router', 'ngCordova' ] )
 	
 	.config( function ( $stateProvider ) {
-		$stateProvider.state( 'contactList', {
-			url:          '/contacts',
-			templateUrl:  'views/contacts/list/contact.list.html',
-			controller:   ContactListCtrl,
-			controllerAs: 'contactListCtrl'
-		} );
+		$stateProvider
+			.state( 'contactList', {
+				url:          '/contacts',
+				templateUrl:  'views/contacts/list/contact.list.html',
+				controller:   ContactListCtrl,
+				controllerAs: 'contactListCtrl'
+			} )
+			
+			.state( 'contactListPin', {
+				url:          '/contacts/pinTo/{id:string}/',
+				templateUrl:  'views/contacts/list/contact.list.html',
+				controller:   ContactListPinCtrl,
+				controllerAs: 'contactListCtrl',
+				resolve:      {
+					event: function ( $stateParams, EventFactory ) {
+						console.log( $stateParams.id );
+						
+						return EventFactory
+							.findEvent( $stateParams.id );
+					}
+				}
+			} );
 	} );
 
 function ContactListCtrl( CordovaFactory, ContactFactory ) {
@@ -40,6 +56,45 @@ function ContactListCtrl( CordovaFactory, ContactFactory ) {
 				
 				CordovaFactory.toast( 'Contact deleted' );
 				
+			} );
+	};
+	
+	$this.init();
+}
+
+function ContactListPinCtrl( event, $state, CordovaFactory, ContactFactory ) {
+	var $this = this;
+	
+	$this.contacts = [];
+	$this.event    = event;
+	
+	$this.init = function () {
+		ContactFactory
+			.findAll()
+			.then( function ( contacts ) {
+				$this.contacts = contacts;
+				
+			} );
+	};
+	
+	$this.deleteContact = function ( contact ) {
+		ContactFactory
+			.deleteContact( contact.id )
+			.then( function () {
+				$this.contacts = $this.contacts.filter( function ( item ) {
+					return item.id !== contact.id;
+				} );
+				
+				CordovaFactory.toast( 'Contact deleted' );
+				
+			} );
+	};
+	
+	$this.pinContactToEvent = function ( contact ) {
+		ContactFactory
+			.assignContactToEvent( contact, $this.event )
+			.then( function () {
+				$state.go( 'eventDetail', { id: $this.event.id } );
 			} );
 	};
 	
